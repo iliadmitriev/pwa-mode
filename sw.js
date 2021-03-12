d44e36685be2129917f6f146b3bcc022b5bf12bd
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-03-12 13:04:47 +0300

    feature service worker * created service worker script * installed it to browser * added assets to cache

--- sw.js
+++ sw.js
@@ -1,5 +1,22 @@
+const staticCacheName = 'static-app-v1'
+
+const assetsUrls = [
+    '/index.html',
+    '/404.html',
+    '/js/app.js',
+    '/style/main.css',
+    '/icons/icon-192x192.png',
+    '/icons/icon-256x256.png',
+    '/icons/icon-384x384.png',
+    '/icons/icon-512x512.png'
+]
+
 self.addEventListener('install', async event => {
-    console.log('install')
+    event.waitUntil(
+        caches.open(staticCacheName).then(
+            cache => cache.addAll(assetsUrls)
+        )
+    )
 })
 
 self.addEventListener('activate', async event => {
